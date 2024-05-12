8e0ad90680437a3b95315ee480f2f0dec2a59048
redbrickgrill <168771732+redbrickgrill@users.noreply.github.com>
2024-05-12 10:14:01 -0600

    Add files via upload

---
+++ controlM.js
@@ -0,0 +1,19 @@
+
+var menuTab = document.getElementById("pedido");
+menuTab.addEventListener("click",function (){
+window.location.href = "https://www.ubereats.com/mx/store/red-brick/owCtCA9jRUmCw4DPi29Glg?diningMode=DELIVERY";
+}); 
+
+menuTab.addEventListener("click",function (){
+    window.location.href = "https://www.ubereats.com/mx/store/red-brick/owCtCA9jRUmCw4DPi29Glg?diningMode=DELIVERY";
+    }); 
+
+var menuPH = document.getElementById("MENU");
+
+menuPH.addEventListener("click",function(){
+    window.location.href = 'menuPhoto.html';
+});
+
+menuPH.addEventListener("click",function(){
+    window.location.href = 'menuPhoto.html';
+});
